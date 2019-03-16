e513356efe0aa12d8697a7b6dbf3cd8320b390f0
Phionanamugga <phionanamugga15@gmail.com>
2019-03-17 00:56:59 +0300

    creates groups [Delivers #164577233]

--- UI/JS/home.js
+++
@@ -1,7 +0,0 @@
-function drop() {
-    document.getElementById("comment-dropdown").classList.toggle("show");
-}
-
-function side(){
-    document.getElementById("side-nav").classList.toggle("display");
-}
